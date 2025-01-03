a88eb2f0db9c54a3f489c4636d43ff69f88abfee
Nguyen Cong Khanh <khanhhunter01@gmail.com>
2025-01-03 22:44:19 +0700

    tạo thẻ thanh toán visa và button thanh toán

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,4 +1,6 @@
+import 'package:drink_app1/components/my_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 
 class PaymentPage extends StatefulWidget {
   const PaymentPage({super.key});
@@ -8,6 +10,13 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  String cardNumber = "";
+  String expiryDate = "";
+  String cardHolderName = "";
+  String cvvCode = "";
+  bool isCvvFocused = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +25,47 @@ class _PaymentPageState extends State<PaymentPage> {
         backgroundColor: Colors.transparent,
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Thanh toán"),
+      ),
+      body: Column(
+        children: [
+          //thanh toan visa
+          CreditCardWidget(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            showBackView: isCvvFocused,
+            onCreditCardWidgetChange: (p0) {},
+          ),
+
+          //form thanh toan
+          CreditCardForm(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            onCreditCardModelChange: (data) {
+              setState(() {
+                cardNumber = data.cardNumber;
+                expiryDate = data.expiryDate;
+                cardHolderName = data.cardHolderName;
+                cvvCode = data.cvvCode;
+              });
+            },
+            formKey: formKey,
+          ),
+
+          const Spacer(),
+
+          MyButton(
+            onTap: () {},
+            // Thực hiện hành động khi nút được nhấn
+
+            text: "Thanh toán ngay",
+          ),
+
+          const SizedBox(height: 25),
+        ],
       ),
     );
   }
